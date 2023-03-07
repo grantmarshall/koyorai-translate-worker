89c3e53c082a7ff23d16bfef1f578441a8f40d11
Grant Marshall <gandrewmarshall@gmail.com>
2023-03-07 16:55:12 -0700

    Finish the loop on the worker so it inserts data into the db

--- koyorai/translation_worker.py
+++ koyorai/translation_worker.py
@@ -9,29 +9,36 @@ import whisper
 
 
 def get_oldest_session(db):
-    return None
+    print('Getting oldest session.')
+    ids = [row[0] for row in db.execute(
+        'SELECT c.session_id AS session_id FROM audio_chunks c'
+        ' ORDER BY c.insert_ts DESC'
+    ).fetchall()]
+    return ids[0]
 
 
 def get_bytes_for_session(db, id):
-    chunks = [bytes(chunk[0]) for chunk in db.execute(
-        'SELECT c.chunk AS chunk FROM audio_chunks c'
+    chunks = []
+    last_chunk_id = -1
+    for chunk in db.execute(
+        'SELECT c.chunk AS chunk, c.id AS audio_chunk_id FROM audio_chunks c'
         ' WHERE c.session_id = ?'
         ' ORDER BY c.user_ts ASC',
         (id, )
-    ).fetchall()]
-    print('Found {} chunks in session {}'.format(len(chunks), id))
-    return sf.read(io.BytesIO(b''.join(chunks)))
+    ).fetchall():
+        chunks.append(bytes(chunk[0]))
+        last_chunk_id = int(chunk[1])
+
+    print('Found {} chunks in session {} with last chunk id {}'
+          .format(len(chunks), id, last_chunk_id))
+    return last_chunk_id, b''.join(chunks)
 
 
 def main():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-db', '--database', type=str)
-    parser.add_argument('-s', '--session', type=str)
     args, _ = parser.parse_known_args()
-    db = sqlite3.connect(
-        args.database,
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
+    db = sqlite3.connect(args.database, detect_types=sqlite3.PARSE_DECLTYPES)
 
     # Create a model to be reused for translation
     print('Loading model...')
@@ -43,31 +50,33 @@ def main():
     tfm.set_output_format(channels=1, rate=16000)
     times = []
 
-    # TODO(grantmarshall) Get the most out of date session from the DB
-    # oldest_session_id = get_oldest_session(db)
-
     # Get raw bytes for the audio of the session as a numpy array
     while True:
-        data, sample_rate = get_bytes_for_session(db, args.session)
+        session_id = get_oldest_session(db)
+        last_chunk_id, session_bytes = get_bytes_for_session(db, session_id)
+        data, sample_rate = sf.read(io.BytesIO(session_bytes))
 
         # Downsample the data and feed it into the translator
         print('Beginning translation.')
         begin_time = time.time()
         downsampled_data = tfm.build_array(
-            input_array=data,
+            input_array=data[-30 * sample_rate:],
             sample_rate_in=sample_rate
         )
         result = whisper.transcribe(
-            audio=downsampled_data[-30 * sample_rate:].astype(numpy.float32),
+            audio=downsampled_data.astype(numpy.float32),
             model=model,
             task="translate")
         elapsed_time = (time.time() - begin_time)
         times.append(elapsed_time)
 
-        # TODO(grantmarshall): Insert the translation into the db
-        print('Computation time: {:2f}s'.format(elapsed_time))
-        print('Running average: {:2f}s'.format(sum(times) / len(times)))
+        print('Computation time: {:03.2f}s'.format(elapsed_time))
+        print('Running average: {:03.2f}s'.format(sum(times) / len(times)))
         print('Result: {}'.format(result['text']))
+        db.execute('INSERT INTO translations (session_id, audio_chunk_id, tl)'
+                   ' VALUES (?, ?, ?)',
+                   (session_id, last_chunk_id, result['text']))
+        db.commit()
 
 
 if __name__ == '__main__':
